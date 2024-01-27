108e4c0adac2d77dab1c25000691c4c349890eae
Noemy Roberta <noemy.oliveira2@outlook.com>
2024-01-27 16:51:14 -0300

    refactor: create `status` route

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use((request, response, next) => {
+app.use('/status',(request, response, next) => {
     response.status(200).send({
         mensagem: "OK",
     });
