767ef2b4306555aae7e0d2574ccc57f05603b2f5
Noemy Roberta <noemy.oliveira2@outlook.com>
2024-01-28 17:11:03 -0300

    feat: create model for car

---
+++ db/models/car.js
@@ -0,0 +1,34 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db_configuration');
+
+const Car = sequelize.define('Car', {
+    id: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+    },
+    brand: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    model: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    year: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    rentalRate: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+    },
+    available: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+    },
+});
+
+module.exports = Car;
