66c9e1ff65132d5b8895abfbc0c7d2dfd6fd8f3b
Noemy Roberta <noemy.oliveira2@outlook.com>
2024-01-28 11:48:07 -0300

    feat: add instance of db in server

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const http = require('http');
 const app = require('./app');
+const db = require('./db/db_configuration');
 const server = http.createServer(app);
 
 const PORT = process.env.PORT || 3000;
 const HOST = 'localhost';
+
+db.sync(() => console.log(`Database connected: ${process.env.DB_NAME}`));
 
 server.listen(PORT, HOST, () => {
     console.log(`Server running at http://${HOST}:${PORT}/`);
