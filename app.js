1993aba5b41376f0729faa0c2f6e352e66846b03
Noemy Roberta <noemy.oliveira2@outlook.com>
2024-01-28 20:57:52 -0300

    feat: instanciate car routing on app

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const serverUrl = require('./routes/utils/server_url');
 const serverRouter = require('./routes/server_router');
+const carUrl = require('./routes/utils/car_url');
+const carRouter = require('./routes/car_router');
 
-app.use(serverUrl.SERVER, serverRouter); 
+app.use(serverUrl.SERVER, serverRouter);
+app.use(carUrl.BASE, carRouter); 
 
 module.exports = app;
