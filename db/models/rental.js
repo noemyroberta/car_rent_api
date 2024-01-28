8a403a6ae90376e15a5e1c9affb00d67aaacfe0e
Noemy Roberta <noemy.oliveira2@outlook.com>
2024-01-28 17:19:06 -0300

    feat: add rental model

---
+++ db/models/rental.js
@@ -0,0 +1,32 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db_configuration');
+const Car = require('./Car');
+const Customer = require('./Customer');
+
+const Rental = sequelize.define('Rental', {
+    id: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+    },
+    startDate: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: Date.now(),
+    },
+    endDate: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    rentAmount: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+        defaultValue: 0.0,
+    }
+});
+
+Rental.belongsTo(Car);
+Rental.belongsTo(Customer);
+
+module.exports = Rental;
