db3b29bc2a7312ea8ae36c30bd0c958bb6f9b95e
Noemy Roberta <noemy.oliveira2@outlook.com>
2024-01-29 16:21:20 -0300

    refactor: import `CarValidator`

--- routes/car_router.js
+++ routes/car_router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const carUrl = require('./utils/car_url');
 const router = express.Router();
+const CarValidator = require('../db/validators/car_validator');
 const CarController = require('../db/controllers/car_controller');
 const CarRepository = require('../db/repositories/car_repository');
 
@@ -17,7 +18,7 @@ router.get(carUrl.GET_ALL, async (_, response, __) => {
 
 router.post(
     carUrl.INSERT,
-    CarController.validate('insertCar'),
+    CarValidator.validate('insertCar'),
     CarController.insert,
 );
 
