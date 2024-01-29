7b54f18c1835b17a2b12719680a9a1a33bb202a3
Noemy Roberta <noemy.oliveira2@outlook.com>
2024-01-29 16:14:31 -0300

    refactor: remove not needed import for validator

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ const serverUrl = require('./routes/utils/server_url');
 const serverRouter = require('./routes/server_router');
 const carUrl = require('./routes/utils/car_url');
 const carRouter = require('./routes/car_router');
-const expressValidator = require('express-validator');
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
-app.use(expressValidator());
 
 app.use(serverUrl.SERVER, serverRouter);
 app.use(carUrl.BASE, carRouter);
