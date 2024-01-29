a2244457d910c299c000eddc9ba088f0f6fb2b9d
Noemy Roberta <noemy.oliveira2@outlook.com>
2024-01-28 21:04:24 -0300

    style: remove unused improt

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const http = require('http');
 const app = require('./app');
 const db = require('./db/db_configuration');
-const CarRepository = require('./db/repositories/car_repository');
 const server = http.createServer(app);
 
 const PORT = process.env.PORT || 3000;
@@ -10,7 +9,6 @@ const HOST = 'localhost';
 try {
     db.authenticate();
     console.log('Connection has been established successfully.');
-
 } catch (error) {
     console.error('Unable to connect to the database:', error);
 }
