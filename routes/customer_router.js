2328cb821200309e1bac0671410ba3492c6a8210
Noemy Roberta <noemy.oliveira2@outlook.com>
2024-01-30 17:46:57 -0300

    feat: create router for customer

---
+++ routes/customer_router.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const customerUrl = require('./utils/customer_url');
+const router = express.Router();
+const CustomerValidator = require('../db/validators/customer_validator');
+const CustomerController = require('../db/controllers/customer_controller');
+
+router.get(
+    customerUrl.GET_ALL,
+    CustomerValidator.validate('getAll'),
+    CustomerController.getAll,
+);
+
+router.post(
+    customerUrl.INSERT,
+    CustomerValidator.validate('insertCustomer'),
+    CustomerController.insert,
+);
+
+router.get(
+    customerUrl.GET_BY_ID,
+    CustomerValidator.validate('getCustomerByUuid'),
+    CustomerController.getByUuid,
+);
+
+module.exports = router;
