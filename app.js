1f7752e111e9b8804bc22bc2e6592ea2ac89ead3
Noemy Roberta <noemy.oliveira2@outlook.com>
2024-01-30 20:03:01 -0300

    feat: use instance of rental router on app

--- app.js
+++ app.js
@@ -6,11 +6,14 @@ const carUrl = require('./routes/utils/car_url');
 const carRouter = require('./routes/car_router');
 const customerUrl = require('./routes/utils/customer_url');
 const customerRouter = require('./routes/customer_router');
+const rentalUrl = require('./routes/utils/rental_url');
+const rentalRouter = require('./routes/rental_router');
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(serverUrl.SERVER, serverRouter);
 app.use(carUrl.BASE, carRouter);
 app.use(customerUrl.BASE, customerRouter);
+app.use(rentalUrl.BASE, rentalRouter);
 
 module.exports = app;
