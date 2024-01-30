1ccbaca7250b45bb3517cb224cf995b59d645ebd
Noemy Roberta <noemy.oliveira2@outlook.com>
2024-01-29 22:20:13 -0300

    feat: add `getByUuid` function

--- db/controllers/car_controller.js
+++ db/controllers/car_controller.js
@@ -1,8 +1,12 @@
 const Car = require('../models/car')
+const CarRepository = require('../repositories/car_repository');
 const { v4: uuidv4 } = require('uuid');
 const { validationResult } = require('express-validator');
 
-const inserted = {message: "Car created successfully!"};
+const repository = new CarRepository();
+
+const inserted = { message: "Car created successfully!" };
+const getted = { message: "Car getted successfully!" };
 
 exports.insert = async (req, res, next) => {
     verifyError(req, res);
@@ -11,13 +15,13 @@ exports.insert = async (req, res, next) => {
     const uuid = uuidv4();
     if (brand && model && year && rentalRate) {
         try {
-            const newCar = await Car.create({
+            const newCar = await repository.insert(Car({
                 uuid,
                 brand,
                 model,
                 year,
-                rentalRate, 
-            });
+                rentalRate,
+            }));
             const jsonResponse = { inserted, car: newCar };
             res.status(201).json(jsonResponse);
         } catch (error) {
@@ -31,5 +35,21 @@ function verifyError(req, res) {
     if (!errors.isEmpty()) {
         res.status(422).json({ errors: errors.array() });
         return;
+    }
+}
+
+
+exports.getByUuid = async (req, res, next) => {
+    verifyError(req, res);
+
+    const { uuid } = req.body;
+    if (uuid) {
+        try {
+            const foundCar = await repository.getByUuid(uuid);
+            const jsonResponse = { getted, car: foundCar };
+            res.status(200).json(jsonResponse);
+        } catch (error) {
+            return next(error);
+        }
     }
 }
