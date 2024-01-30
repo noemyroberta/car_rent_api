1bc808d93bdf2181756ce76f5221831165167c4a
Noemy Roberta <noemy.oliveira2@outlook.com>
2024-01-30 17:50:27 -0300

    feat: add instance router for customer

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ const serverUrl = require('./routes/utils/server_url');
 const serverRouter = require('./routes/server_router');
 const carUrl = require('./routes/utils/car_url');
 const carRouter = require('./routes/car_router');
+const customerUrl = require('./routes/utils/customer_url');
+const customerRouter = require('./routes/customer_router');
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(serverUrl.SERVER, serverRouter);
 app.use(carUrl.BASE, carRouter);
+app.use(customerUrl.BASE, customerRouter);
 
 module.exports = app;
