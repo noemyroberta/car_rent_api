f623e112c7409e6c4fa2e75932414eeec1849d1c
Noemy Roberta <noemy.oliveira2@outlook.com>
2024-01-31 11:31:19 -0300

    refactor: update validator for date

--- db/validators/rental_validator.js
+++ db/validators/rental_validator.js
@@ -1,10 +1,10 @@
 const { body, param } = require('express-validator');
 exports.validate = (method) => {
     switch (method) {
-
         case 'rentCar': {
             return [
-                body('endDate', 'endDate is required').exists(),
+                body('endDate', 'endDate is required').exists().if(body('endDate').exists()).isISO8601() // Checks if the attribute is a valid ISO 8601 date
+                    .custom((value) => isDateOnly(value)),
                 body('carUuid', 'carUuid is required').exists().if(body('carUuid').exists()).isString(),
                 body('customerUuid', 'customerUuid is required').exists().if(body('customerUuid').exists()).isString(),
             ]
@@ -29,3 +29,8 @@ exports.validate = (method) => {
         }
     }
 }
+
+const isDateOnly = (value) => {
+    const iso8601Regex = /^\d{4}-\d{2}-\d{2}$/;
+    return iso8601Regex.test(value);
+};
