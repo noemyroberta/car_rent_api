9d60e661b11538780cbe87d47a7d46bec2dca106
Noemy Roberta <noemy.oliveira2@outlook.com>
2024-02-03 13:41:23 -0300

    feat: run insertion sql scripts by default

--- db/db_configuration.js
+++ db/db_configuration.js
@@ -2,11 +2,17 @@ const { Sequelize } = require("sequelize");
 const fs = require('fs');  
 
 const sqlCarInserts = fs.readFileSync('./data/insert_car.sql', 'utf8');
+const sqlCustomerInserts = fs.readFileSync('./data/insert_customer.sql', 'utf8');
 
 const sequelize = new Sequelize({
   dialect: "sqlite",
   storage: "./car_rent.db",
 });
 
-sequelize.query(sqlInserts);
+const resultCarInsert = sequelize.query(sqlCarInserts);
+const resultCustomerInsert = sequelize.query(sqlCustomerInserts);
+
+console.log(resultCarInsert);
+console.log(resultCustomerInsert);
+
 module.exports = sequelize;
