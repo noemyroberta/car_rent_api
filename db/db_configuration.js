0d176ee6d4db645f961bc31b02778e3d53bdd991
Noemy Roberta <noemy.oliveira2@outlook.com>
2024-02-03 12:31:36 -0300

    refactor: replace db config to `sqlite`

--- db/db_configuration.js
+++ db/db_configuration.js
@@ -1,14 +1,8 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config()
-const dbName = process.env.DB_NAME;
-const dbUser = process.env.DB_USER;
-const dbHost = process.env.DB_HOST;
-const dbPassword = process.env.DB_PASSWORD;
+const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
-  dialect: "mysql", 
-  host: dbHost, 
-  logging: console.log,
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: "./car_rent.db",
 });
 
 module.exports = sequelize;
