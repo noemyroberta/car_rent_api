47e4924a74c30048473702d21c97c961234b5693
Noemy Roberta <noemy.oliveira2@outlook.com>
2024-02-03 13:39:02 -0300

    feat: create `sql` insertion for custommer

---
+++ db/data/insert_customer.sql
@@ -0,0 +1,76 @@
+
+INSERT INTO db_car_rent.tb_customer (`firstName`, `lastName`, `email`)
+VALUES ("John", "Doe", "john.doe@example.com");
+
+
+INSERT INTO db_car_rent.tb_customer (`firstName`, `lastName`, `email`)
+VALUES ("Jane", "Smith", "jane.smith@example.com");
+
+
+INSERT INTO db_car_rent.tb_customer (`firstName`, `lastName`, `email`)
+VALUES ("Michael", "Johnson", "michael.johnson@example.com");
+
+
+INSERT INTO db_car_rent.tb_customer (`firstName`, `lastName`, `email`)
+VALUES ("Emily", "Williams", "emily.williams@example.com");
+
+
+INSERT INTO db_car_rent.tb_customer (`firstName`, `lastName`, `email`)
+VALUES ("Robert", "Brown", "robert.brown@example.com");
+
+
+INSERT INTO db_car_rent.tb_customer (`firstName`, `lastName`, `email`)
+VALUES ("Sophia", "Jones", "sophia.jones@example.com");
+
+
+INSERT INTO db_car_rent.tb_customer (`firstName`, `lastName`, `email`)
+VALUES ("William", "Miller", "william.miller@example.com");
+
+
+INSERT INTO db_car_rent.tb_customer (`firstName`, `lastName`, `email`)
+VALUES ("Olivia", "Davis", "olivia.davis@example.com");
+
+
+INSERT INTO db_car_rent.tb_customer (`firstName`, `lastName`, `email`)
+VALUES ("James", "Anderson", "james.anderson@example.com");
+
+
+INSERT INTO db_car_rent.tb_customer (`firstName`, `lastName`, `email`)
+VALUES ("Emma", "Wilson", "emma.wilson@example.com");
+
+
+INSERT INTO db_car_rent.tb_customer (`firstName`, `lastName`, `email`)
+VALUES ("Alexander", "Martin", "alexander.martin@example.com");
+
+
+INSERT INTO db_car_rent.tb_customer (`firstName`, `lastName`, `email`)
+VALUES ("Isabella", "Thompson", "isabella.thompson@example.com");
+
+
+INSERT INTO db_car_rent.tb_customer (`firstName`, `lastName`, `email`)
+VALUES ("Daniel", "White", "daniel.white@example.com");
+
+
+INSERT INTO db_car_rent.tb_customer (`firstName`, `lastName`, `email`)
+VALUES ("Ava", "Garcia", "ava.garcia@example.com");
+
+
+INSERT INTO db_car_rent.tb_customer (`firstName`, `lastName`, `email`)
+VALUES ("Liam", "Martinez", "liam.martinez@example.com");
+
+
+INSERT INTO db_car_rent.tb_customer (`firstName`, `lastName`, `email`)
+VALUES ("Mia", "Robinson", "mia.robinson@example.com");
+
+
+INSERT INTO db_car_rent.tb_customer (`firstName`, `lastName`, `email`)
+VALUES ("Ethan", "Hall", "ethan.hall@example.com");
+
+
+INSERT INTO db_car_rent.tb_customer (`firstName`, `lastName`, `email`)
+VALUES ("Grace", "Young", "grace.young@example.com");
+
+
+INSERT INTO db_car_rent.tb_customer (`firstName`, `lastName`, `email`)
+VALUES ("Logan", "Clark", "logan.clark@example.com");
+    
