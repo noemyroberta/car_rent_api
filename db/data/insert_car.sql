00bb53a77c9068e721c29a7bc1a704a02143866b
Noemy Roberta <noemy.oliveira2@outlook.com>
2024-02-03 13:36:38 -0300

    feat: add `sql` to insert cars

---
+++ db/data/insert_car.sql
@@ -0,0 +1,74 @@
+INSERT INTO db_car_rent.tb_car (`brand`, `model`, `year`, `rentalRate`)
+VALUES ('Honda', 'Acoord', 2021, 55.00);
+
+
+INSERT INTO db_car_rent.tb_car (`brand`, `model`, `year`, `rentalRate`)
+VALUES ("Ford", "Fusion", 2019, 48.50);
+
+
+INSERT INTO db_car_rent.tb_car (`brand`, `model`, `year`, `rentalRate`)
+VALUES ("Chevrolet", "Malibu", 2022, 52.75);
+
+
+INSERT INTO db_car_rent.tb_car (`brand`, `model`, `year`, `rentalRate`)
+VALUES ("Nissan", "Altima", 2018, 45.80);
+
+
+INSERT INTO db_car_rent.tb_car (`brand`, `model`, `year`, `rentalRate`)
+VALUES ("Hyundai", "Sonata", 2020, 49.25);
+
+
+INSERT INTO db_car_rent.tb_car (`brand`, `model`, `year`, `rentalRate`)
+VALUES ("Volkswagen", "Passat", 2017, 44.90);
+
+
+INSERT INTO db_car_rent.tb_car (`brand`, `model`, `year`, `rentalRate`)
+VALUES ("BMW", "3 Series", 2022, 60.50);
+
+
+INSERT INTO db_car_rent.tb_car (`brand`, `model`, `year`, `rentalRate`)
+VALUES ("Mercedes-Benz", "C", 2000, 58.75);
+
+
+INSERT INTO db_car_rent.tb_car (`brand`, `model`, `year`, `rentalRate`)
+VALUES ("Audi", "A4", 2020, 56.20);
+
+
+INSERT INTO db_car_rent.tb_car (`brand`, `model`, `year`, `rentalRate`)
+VALUES ("Lexus", "ES", 2021, 59.90);
+
+
+INSERT INTO db_car_rent.tb_car (`brand`, `model`, `year`, `rentalRate`)
+VALUES ("Mazda", "6", 2018, 46.30);
+
+
+INSERT INTO db_car_rent.tb_car (`brand`, `model`, `year`, `rentalRate`)
+VALUES ("Subaru", "Legacy", 2021, 53.40);
+
+
+INSERT INTO db_car_rent.tb_car (`brand`, `model`, `year`, `rentalRate`)
+VALUES ("Kia", "Optima", 2019, 47.80);
+
+
+INSERT INTO db_car_rent.tb_car (`brand`, `model`, `year`, `rentalRate`)
+VALUES ("TeslaModel", "S", 2022, 90.00);
+
+
+INSERT INTO db_car_rent.tb_car (`brand`, `model`, `year`, `rentalRate`)
+VALUES ("Jaguar", "XE", 2020, 65.25);
+
+
+INSERT INTO db_car_rent.tb_car (`brand`, `model`, `year`, `rentalRate`)
+VALUES ("Volvo", "S60", 2017, 49.90);
+
+
+INSERT INTO db_car_rent.tb_car (`brand`, `model`, `year`, `rentalRate`)
+VALUES ("Chrysler", "300", 2021, 54.75);
+
+
+INSERT INTO db_car_rent.tb_car (`brand`, `model`, `year`, `rentalRate`)
+VALUES ("Buick", "Regal", 2018, 48.60);
+
+
+INSERT INTO db_car_rent.tb_car (`brand`, `model`, `year`, `rentalRate`)
+VALUES ("Infiniti", "Q50", 2022, 62.00);
--- db/db_configuration.js
+++ db/db_configuration.js
@@ -1,8 +1,12 @@
 const { Sequelize } = require("sequelize");
+const fs = require('fs');  
+
+const sqlCarInserts = fs.readFileSync('./data/insert_car.sql', 'utf8');
 
 const sequelize = new Sequelize({
   dialect: "sqlite",
   storage: "./car_rent.db",
 });
 
+sequelize.query(sqlInserts);
 module.exports = sequelize;
